6afc1075209106aa4e67ab9cd83e129695521279
Ashwin <ashwinr2k2@gmail.com>
2020-02-22 01:07:08 +1100

    idk

--- ashwin/details_screen.py
+++ ashwin/details_screen.py
@@ -1,7 +1,6 @@
 import pygame
 import threading
 from button import *
-from text_input import * 
 from animate import animate
 import time
 import bg
@@ -11,15 +10,20 @@ class details_screen():
         self.bg = bg
         self.display = DISPLAY
         self.font = pygame.font.SysFont('Arial', 50)
+        self.num_players = 0
 
+        left_btn = button([255,255,255,100], [255,255, 255,190], (1*DISPLAY.get_width()/2) - 125, (1 * DISPLAY.get_height()/2) - 25, 50, 50, "-")
+
+        right_btn = button([255,255,255,100], [255,255, 255,190], (1*DISPLAY.get_width()/2) + 75, (1 * DISPLAY.get_height()/2) - 25, 50, 50, "+")
 
         
+        self.disp_num = self.font.render(str(self.num_players), True, (255,255,255))
 
         #exit button
         self.exit_btn = button([230,230,230,100],[180,180,180,190], DISPLAY.get_width() - 60, 10, 50, 50, "X")
 
         #heading
-        self.heading = self.font.render("Snakes & Ladders", True, (255,255,255))
+        self.heading = self.font.render("Number of Players", True, (255,255,255))
         
 
         self.mp_btn = button([230,230,230,100], [180,180,180,190], (DISPLAY.get_width()/2) -100, (DISPLAY.get_height() * 3/4) +5, 200,100, "Continue")
@@ -31,6 +35,7 @@ class details_screen():
 
             #heading
             self.display.blit(self.heading, ((self.display.get_width() / 2) - (self.heading.get_width() / 2),(self.display.get_height()/4) - (self.heading.get_height() / 2)))
+            self.display.blit(self.disp_num, ((self.display.get_width()/2) - (self.disp_num.get_width()/2), (self.display.get_height()/2) - (self.disp_num.get_height()/2)))
 
             self.exit_btn.draw(self.display)
             self.mp_btn.draw(self.display)
